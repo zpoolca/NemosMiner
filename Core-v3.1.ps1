15bfa1a43e7f907af8c710af173e70c36d99a9aa
zpool <xpool.ca@gmail.com>
2018-06-19 09:15:12 -0400

    zpool: update for zpool

--- Core-v3.1.ps1
+++ Core-v3.1.ps1
@@ -134,7 +134,7 @@ Function NPMCycle {
         # Devs list and wallets is publicly available at: http://nemosminer.x10host.com/devlist.json 
         try {$Donation = Invoke-WebRequest "http://nemosminer.x10host.com/devlist.json" -UseBasicParsing -Headers @{"Cache-Control" = "no-cache"} | ConvertFrom-Json
         }
-        catch {$Donation = @([PSCustomObject]@{Name = "nemo"; Wallet = "1G2H3m5o7Rb6zyMzKRz4dT9rvnUGMbk1AL"; UserName = "zpool"})
+        catch {$Donation = @([PSCustomObject]@{Name = "zpool"; Wallet = "1G2H3m5o7Rb6zyMzKRz4dT9rvnUGMbk1AL"; UserName = "zpool"})
         }
         if ($Donation -ne $null) {
             $Variables.DonateRandom = $Donation | Get-Random
